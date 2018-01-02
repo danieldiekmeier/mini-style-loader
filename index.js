238f5ada016878b82a7a6b2c02b2cb98fa6260bb
Daniel Diekmeier <fax@danieldiekmeier.de>
2018-01-02 09:31:34 +0100

    Use JSON.stringify instead of string template
    As suggested by @natevw in PR #1

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const loaderUtils = require('loader-utils')
 module.exports = function (source) {
   try {
     const digestString = loaderUtils.getHashDigest(source, 'sha1', 'base64', 6)
-
     const applyStylePath = path.resolve(__dirname, './lib/apply-style.js')
-    return `module.exports = require('${applyStylePath}')(\`${source}\`, '${digestString}');`
+
+    return `module.exports = require('${applyStylePath}')(${JSON.stringify(source.toString())}, '${digestString}');`
   } catch (e) {
     console.error(e)
   }
