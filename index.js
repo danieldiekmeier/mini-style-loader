92df8f3906a694ce1582809942ac8bc2697633f5
Daniel Diekmeier <fax@danieldiekmeier.de>
2018-08-24 18:00:43 +0200

    Added Hot Reloading

--- index.js
+++ index.js
@@ -3,13 +3,42 @@ const loaderUtils = require('loader-utils')
 
 module.exports = function (source) {
   try {
-    const digestString = loaderUtils.getHashDigest(source, 'sha1', 'base64', 6)
-    const applyStylePath = path.resolve(__dirname, './lib/apply-style.js')
+    const digestString = loaderUtils.getHashDigest(Buffer.from(source), 'sha1', 'base64', 6)
+    let applyStylePath = path.resolve(__dirname, './lib/apply-style.js')
 
-    return `module.exports = require('${applyStylePath}')(${JSON.stringify(source.toString())}, '${digestString}');`
+    return `
+      import apply from '${applyStylePath}';
+      export default apply(${JSON.stringify(source)}, '${digestString}');
+    `
   } catch (e) {
     console.error(e)
   }
 }
 
-module.exports.raw = true
+module.exports.pitch = function (request) {
+  if (!request.includes('extractLoader.js')) {
+    throw new Error('Mini Style Loader can only be used after ExtractLoader: https://github.com/peerigon/extract-loader')
+  }
+
+  if (this.hot) {
+    const applyStylePath = path.resolve(__dirname, './lib/apply-style-hot.js')
+
+    const filePath = loaderUtils.stringifyRequest(this, '!!' + (request.split('extractLoader.js!')[1]))
+
+    const hash = JSON.stringify(loaderUtils.getHashDigest(Buffer.from(filePath), 'sha1', 'base64', 6))
+
+    return `
+      import apply from '${applyStylePath}'
+
+      // ExtractLoader handles getting the right source, but puts it in [0][1]
+      const source = require(${filePath})[0][1];
+
+      export default apply(source, ${hash})
+
+      module.hot.accept(${filePath}, () => {
+        const source = require(${filePath})[0][1];
+        apply(source, ${hash})
+      })
+    `
+  }
+}
